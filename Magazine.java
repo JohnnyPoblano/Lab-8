07cb1b33934646501c232b099ab6372baa0eb6ea
John <johncgumm@gmail.com>
2018-11-17 22:42:56 -0700

    Added .java files

---
+++ Magazine.java
@@ -0,0 +1,9 @@
+/*******************
+** John Gumm
+** CIS 131
+** Lab 8 
+*******************/
+
+public class Magazine extends Publication {
+    
+}
---
+++ Newsletter.java
@@ -0,0 +1,9 @@
+/*******************
+** John Gumm
+** CIS 131
+** Lab 8 
+*******************/
+
+public class Newsletter extends Publication {
+    
+}
